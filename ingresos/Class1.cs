ccbfc88dc972331eeb173834b2d1d2d85d1fc487
Cupsuki001 <124387400+Cupsuki001@users.noreply.github.com>
2024-02-01 17:39:15 -0600

    Add files via upload

---
+++ ingresos/Class1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ingresos
+{
+    internal class Class1
+    {
+        double a , b , u;
+
+        public Class1 () { }
+        public Class1(double a , double b) 
+        {
+          this.a = a;
+            this.b = b;
+        }
+
+        public void util(double a, double b) 
+        {
+            u = a - b;
+        }
+
+        public void rest()
+        {
+            Console.WriteLine("La utilidad es: "+ u);
+        }
+        public int validar(String Mensaje)
+        {
+            int num = 0, v;
+            do
+            {
+                v = 1;
+                try
+                {
+                    Console.Write(Mensaje);
+                    num = Convert.ToInt32(Console.ReadLine());
+                }
+                catch (Exception error)
+                {
+                    v = 0;
+                    Console.WriteLine("////////////////////////////////////");
+                    Console.WriteLine("No se aceptan letras", error.Message);
+                    Console.WriteLine("////////////////////////////////////");
+                  
+                }
+            } while (v == 0);
+            return num;
+        }
+    }
+}
---
+++ ingresos/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ingresos;
+
+public class trabajo 
+{
+  public static void Main(string[] args) 
+  {
+        double ig , eg , tt;
+        Class1 v = new Class1();
+
+        do 
+        {
+            ig = v.validar("Digite su Ingreso: ");
+            Console.WriteLine("------------------//");
+
+        }while (ig < 1000 || ig > 50000 );
+
+        do
+        {
+            eg = v.validar("Digite su egreso: ");
+            Console.WriteLine("------------------//");
+
+        } while (eg >= ig);
+
+         v.util(ig,eg);
+         v.rest();
+
+
+    }
+}
